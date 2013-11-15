0fb1d898274a3a6e5c7d53d8df1cf69016a6062e
NoSFeRaTU <master.nosferatu@gmail.com>
2013-11-15 18:04:12 +0400

    added forgotten libdvbcsa files for commit 6788c84c2f

---
+++ modules/softcam/libdvbcsa/config.h
@@ -0,0 +1,26 @@
+#define HAVE_ASSERT_H 1
+#define HAVE_DLFCN_H 1
+#define HAVE_INTTYPES_H 1
+#define HAVE_MEMORY_H 1
+#define HAVE_MM_MALLOC 1
+#define HAVE_STDINT_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_STRINGS_H 1
+#define HAVE_STRING_H 1
+#define HAVE_SYS_STAT_H 1
+#define HAVE_SYS_TYPES_H 1
+#define HAVE_UNISTD_H 1
+#define LT_OBJDIR ".libs/"
+#define PACKAGE "libdvbcsa"
+#define PACKAGE_BUGREPORT ""
+#define PACKAGE_NAME "libdvbcsa"
+#define PACKAGE_STRING "libdvbcsa 1.1.0"
+#define PACKAGE_TARNAME "libdvbcsa"
+#define PACKAGE_URL ""
+#define PACKAGE_VERSION "1.1.0"
+#define SIZEOF_LONG 8
+#define STDC_HEADERS 1
+#define VERSION "1.1.0"
+#ifndef __cplusplus
+/* #undef inline */
+#endif
